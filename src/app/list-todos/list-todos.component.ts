ca6da696b125d8243c2bffb360f0b92c6eafcd69
Paul <pacalabre@gmail.com>
2023-03-27 14:34:15 -0600

    Added component functionality and mark up for todo list and todo items.

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
-import { Output, EventEmitter } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css'],
 })
-export class ListTodosComponent {}
+export class ListTodosComponent {
+  @Input() listOfTodos: Array<{
+    id: string;
+    name: string;
+    isComplete: boolean;
+  }> = [];
+  @Output() removeItemEvent = new EventEmitter();
+  @Output() todoNameUpdated = new EventEmitter();
+  @Output() todoIsCompleteUpdated = new EventEmitter();
+
+  removeItem(id: number) {
+    this.removeItemEvent.emit(id);
+  }
+
+  updateName(value: any, id: string) {
+    this.todoNameUpdated.emit({ name: value, id: id });
+  }
+
+  updateIsComplete(value: boolean, id: string) {
+    this.todoIsCompleteUpdated.emit({ isComplete: value, id: id });
+  }
+}
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-todo',
+  selector: 'todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
+  @Input() name: string = '';
+  @Input() isComplete: boolean = false;
+  @Output() editedName = new EventEmitter();
+  @Output() isCompleteChanged = new EventEmitter();
+  @Output() deleteTodo = new EventEmitter();
+  isEditing: boolean = false;
 
+  toggleEdit() {
+    this.isEditing = !this.isEditing;
+  }
+
+  toggleIsComplete() {
+    this.isComplete = !this.isComplete;
+    this.isCompleteChanged.emit(this.isComplete);
+  }
+
+  editName(event: any) {
+    this.editedName.emit(event?.target.value);
+  }
+
+  removeItem() {
+    this.deleteTodo.emit(true);
+  }
 }
